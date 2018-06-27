e3edb0f0746c030255c76036ac254f6dbd6cd5bf
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-06-26 20:44:11 -0600

    Reverses card number

--- credit_check.rb
+++ credit_check.rb
@@ -1,7 +1,15 @@
 card_number = "4929735477250543"
 
 # Your Luhn Algorithm Here
-
+## reverse card number
+card_number.reverse
+## Double every other number
+card_number.chars.each 
+## if the sum is more than 10 sum the products
+## % by 10
+## if 0, valid
+## else, invalid
 # Output
+
 ## If it is valid, print "The number is valid!"
 ## If it is invalid, print "The number is invalid!"
